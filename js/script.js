53b46f83acbbf3648dc86c70e240b8d1d935dd31
ayanna_m <ayanna.m89@gmail.com>
2023-06-23 20:33:29 -0500

    created functions and new global variables

--- js/script.js
+++ js/script.js
@@ -1,31 +1,71 @@
 // Global variables for selected elements
-const guessedLettersList = document.querySelector(".guessed-letters");
-const guessButton = document.querySelector(".guess-button");
-const guessInput = document.querySelector(".guess-input");
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remainingGuesses = document.querySelector(".remaining-guesses");
-const remainingGuessesSpan = document.querySelector(".remaining-guesses-span");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining-span");
 const message = document.querySelector(".message");
-const playAgainButton = document.querySelector(".play-again-button");
+const playAgainButton = document.querySelector(".play-again");
 
 // Starting word
 const word = "magnolia";
+const guessedLetters = [];
 
-const displayWordInProgress = function(word) {
-  const wordArray = Array.from(word); // Convert word string to an array of characters
-  const hiddenWord = wordArray.map(() => "●"); // Create an array of circle symbols (●) with the same length as the word
-  const hiddenWordString = hiddenWord.join(""); // Join the array back to a string
+// Display our symbols as placeholders for the chosen word's letters
 
-  wordInProgress.innerText = hiddenWordString; // Update the innerText of wordInProgress with the hidden word symbols
+const displayWordInProgress = function (word) {
+    const wordArray = Array.from(word); // Convert word string to an array of characters
+    const hiddenWord = wordArray.map(() => "●"); // Create an array of circle symbols (●) with the same length as the word
+    const hiddenWordString = hiddenWord.join(""); // Join the array back to a string
+
+    wordInProgress.innerText = hiddenWordString; // Update the innerText of wordInProgress with the hidden word symbols
 };
 
 // Call the function and pass it the word variable
 displayWordInProgress(word);
 
-guessButton.addEventListener("click", function(e) {
-  e.preventDefault(); // Prevent the default form behavior
+guessButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  // Empty message paragraph
+  message.innerText = "";
+  
+  const guess = letterInput.value;
+  
+  const goodGuess = validateInput(guess);
 
-  const inputValue = guessInput.value; // Capture the value of the input
-  console.log(inputValue); // Log out the captured input value
-  guessInput.value = ""; // Empty the value of the input
+  if (goodGuess) {
+    
+    makeGuess(guess);
+  }
+  letterInput.value = "";
 });
+
+
+const validateInput = function (input) {
+        const acceptedLetter = /[a-zA-Z]/; // Regular expression to match a single alphabetical character
+
+        if (input === "") {
+            // Empty input
+            console.log("Please enter a letter.");
+        } else if (input.length > 1) {
+            // More than one letter
+            console.log("Please enter only one letter.");
+        } else if (!input.match(acceptedLetter)) {
+            // Non-letter character
+            console.log("Please enter a valid letter.");
+        } else {
+            // Valid input: Single alphabetical character
+            return input;
+        }
+    };
+
+    const makeGuess = function (guess) {
+        guess = guess.toUpperCase();
+        if (guessedLetters.includes(guess)) {
+            message.innerText = "You already guessed that letter, silly. Try again.";
+        } else {
+            guessedLetters.push(guess);
+            console.log(guessedLetters);
+        }
+    };
